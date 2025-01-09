10229e36069b75694315abaad7d79841a2e4e2ca
jfk6725 <jfk6725@gmai.com>
2025-01-09 20:14:32 +0900

    first commit

---
+++ src/main/java/com/KoreaItAcdemy/OurNotion/WwwApplication.java
@@ -0,0 +1,13 @@
+package com.KoreaItAcdemy.OurNotion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WwwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WwwApplication.class, args);
+	}
+
+}
